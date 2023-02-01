eba163e7bb687a3ddd5998289528eab4edcfc322
TheDemon12345678910 <anehoimark@gmail.com>
2023-02-01 09:13:08 +0100

    fullScreen

--- src/dk/easv/Main.java
+++ src/dk/easv/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("presentation/AppTile.fxml"));
         primaryStage.setTitle("Movie Recommendation System 0.01 Beta");
+        primaryStage.setFullScreen(true);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
