270f38b9a1265b799b426c9fe9c8801e59cf6928
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-02-07 13:11:15 +0100

    learning how to javaFX

---
+++ src/dk/easv/presentation/controller/IntroScreenController.java
@@ -0,0 +1,13 @@
+package dk.easv.presentation.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IntroScreenController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
