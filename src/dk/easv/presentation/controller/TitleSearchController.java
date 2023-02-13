7e5ca158791519bb22169f2d75aa93ec67dd1bd2
Julia Ilasova <1julka1il@gmail.com>
2023-02-13 13:40:00 +0100

    push!

---
+++ src/dk/easv/presentation/controller/TitleSearchController.java
@@ -0,0 +1,12 @@
+package dk.easv.presentation.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+
+public class TitleSearchController {
+    @FXML private TextField txtSearchBar;
+    @FXML private FlowPane flowPane;
+
+
+}
