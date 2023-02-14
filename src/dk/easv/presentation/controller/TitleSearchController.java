b9a39c35263ac9fdfd056365085e13becbab7840
Julia Ilasova <1julka1il@gmail.com>
2023-02-14 22:36:32 +0100

    more movies now load on scrolling down, searching currently possible after pressing the enter key

--- src/dk/easv/presentation/controller/TitleSearchController.java
+++ src/dk/easv/presentation/controller/TitleSearchController.java
@@ -13,6 +13,7 @@ import javafx.scene.Node;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 
@@ -23,35 +24,39 @@ public class TitleSearchController implements Initializable {
     @FXML private TextField txtSearchBar;
     @FXML private FlowPane flowPane;
     @FXML private ScrollPane scrollPane;
+    private ScrollBar scrollBar;
     private AppModel model;
     private MovieViewFactory movieViewFactory;
     private ObservableList<Movie> filteredMovies = FXCollections.observableArrayList();
-    private ScrollBar scrollBar;
-    private ObservableList<Movie> recommendedMovies = FXCollections.observableArrayList();
+    private ObservableList<HBox> shownMovies = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle resources){
-        recommendedMovies.setAll(model.getTopAverageRatedMoviesUserDidNotSee(model.getObsLoggedInUser()));
+        scrollBar = getVerticalScrollbar(scrollPane);
+        scrollPane.vvalueProperty().addListener(this::scrolled);
+
+        flowPane.minWidthProperty().bind(scrollPane.widthProperty());
+        flowPane.minHeightProperty().bind(scrollPane.heightProperty());
+
+        filteredMovies.setAll(model.getTopAverageRatedMoviesUserDidNotSee(model.getObsLoggedInUser()));
+        setInitialMovies();
 
         txtSearchBar.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredMovies = searchMovies(txtSearchBar.getText().trim().toLowerCase());
+            if (txtSearchBar.getText().isEmpty()){
+                shownMovies.clear();
+                filteredMovies.setAll(model.getTopAverageRatedMoviesUserDidNotSee(model.getObsLoggedInUser()));
+                setInitialMovies();
+            }
         });
 
-        //TODO look into threading
-        if (recommendedMovies.size() > 15){
-            for (int i = 0; i < 15; i++) {
-                flowPane.getChildren().add(movieViewFactory.constructMovieView(recommendedMovies.get(i)));
+        txtSearchBar.setOnKeyReleased(event -> {
+            if(event.getCode() == KeyCode.ENTER){
+                shownMovies.clear();
+                scrollPane.setVvalue(0);
+                filteredMovies = searchMovies(txtSearchBar.getText().trim().toLowerCase());
+                setInitialMovies();
             }
-        }
-        else{
-            for (Movie movie: recommendedMovies){
-                flowPane.getChildren().add(movieViewFactory.constructMovieView(movie));
-            }
-        }
-
-        scrollBar = getVerticalScrollbar(scrollPane);
-        //scrollBar.valueProperty().addListener(this::scrolled);
-
+        });
 
         //TODO figure this out
         //HBox hbox = (HBox) flowPane.getChildren().get(0);
@@ -64,18 +69,6 @@ public class TitleSearchController implements Initializable {
         ObservableList<Movie> filteredMovies = FXCollections.observableArrayList();
         filteredMovies.addAll(movies);
         return filteredMovies;
-    }
-
-    public void setAppModel(AppModel model) {
-        this.model = model;
-    }
-
-    public void setMovieViewFactory(MovieViewFactory movieViewFactory) {
-        this.movieViewFactory = movieViewFactory;
-    }
-
-    public void setInitialItems(List<HBox> movieViews){
-        flowPane.getChildren().setAll(movieViews);
     }
 
     private ScrollBar getVerticalScrollbar(ScrollPane scrollPane) {
@@ -93,16 +86,60 @@ public class TitleSearchController implements Initializable {
 
     void scrolled(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
         double value = newValue.doubleValue();
-        System.out.println("Scrolled to " + value);
         ScrollBar bar = getVerticalScrollbar(scrollPane);
 
-        /*
         if (value == bar.getMax()) {
-            double targetValue = value * filteredMovies.size();
-            movieViewFactory.populateFlowPaneWithMovies(filteredMovies.subList(0,15));
-            bar.setValue(targetValue / filteredMovies.size());
+            double targetValue = value * shownMovies.size();
+            addMovies();
+            bar.setValue(targetValue / shownMovies.size());
         }
+    }
 
-         */
+    private void addMovies(){
+        int size = filteredMovies.size();
+
+        if (size > 0){
+            if (size >= 6){
+                for (int i = 0; i < 6; i++) {
+                    shownMovies.add(movieViewFactory.constructMovieView(filteredMovies.get(0)));
+                    filteredMovies.remove(0);
+                }
+            }
+            else{
+                for (int i = 0; i < size; i++){
+                    shownMovies.add(movieViewFactory.constructMovieView(filteredMovies.get(0)));
+                    filteredMovies.remove(0);
+                }
+            }
+            flowPane.getChildren().setAll(shownMovies);
+        }
+    }
+
+    private void setInitialMovies() {
+        int size = filteredMovies.size();
+
+        if (size > 0){
+            if (size >= 12){
+                for (int i = 0; i < 12; i++) {
+                    shownMovies.add(movieViewFactory.constructMovieView(filteredMovies.get(0)));
+                    filteredMovies.remove(0);
+                }
+            }
+            else{
+                for (int i = 0; i < size; i++){
+                    shownMovies.add(movieViewFactory.constructMovieView(filteredMovies.get(0)));
+                    filteredMovies.remove(0);
+                }
+            }
+        }
+        flowPane.getChildren().setAll(shownMovies);
+    }
+
+    public void setAppModel(AppModel model) {
+        this.model = model;
+    }
+
+    public void setMovieViewFactory(MovieViewFactory movieViewFactory) {
+        this.movieViewFactory = movieViewFactory;
     }
 }
